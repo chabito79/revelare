d5bde25fbcd6e80e8df0cd29af360257bde2802d
Tropicalrambler <alainber@gmail.com>
2019-12-02 06:04:12 +0000

    Clarifying functionality before fetching data chunks

--- revelare/revelare/report/direct_cash_flow_report/direct_cash_flow_report.py
+++ revelare/revelare/report/direct_cash_flow_report/direct_cash_flow_report.py
@@ -16,19 +16,27 @@ from erpnext.stock.report.stock_balance.stock_balance import (get_stock_ledger_e
 from erpnext.accounts.utils import get_fiscal_year
 from six import iteritems
 
-
-
 def execute(filters=None):
 	'''Funcion especial ejecutada por frappe para generar la data para el reporte'''
-
-	new_path = "/home/alain/sihaysistema/apps/revelare/revelare/revelare/report/direct_cash_flow_report/hello_world.txt"
-	accts_file = open(new_path,'w')
-	title = "hola mundo"
-	accts_file.write(title)
-	accts_file.close()
-
+	
+	# Find journal entries for specified time period
+	# Find journal entry accounts that belong to the time period filtered journal entries
+	# From journal entry accounts, Create a list of journal entry names and dates of journal
+	
+	get_journal_entries()
+	get_payment_entries()
+	
+	
+	# DEV  Getting filters to verify
 	filters = frappe._dict(filters or {})
+	# DEV get the dates in datetime object form
+	dev_from_date, dev_to_date = getdate(filters.from_date), getdate(filters.to_date)
+	# DEV Write the file to see what we are getting.
+	# write_file(dev_from_date, 'develop_filters')
+	write_file(get_period_date_ranges(filters), 'develop_date_ranges')
+	
 	columns = get_columns(filters)
+	write_file(columns, 'develop_columns')
 
 	data = [{
 		"total": 0,
@@ -67,18 +75,83 @@ def execute(filters=None):
 
 	return columns, data, None, chart
 
+@frappe.whitelist()
+def get_payment_entries():
+	'''Function that obtains the payment entries from the database'''
+	# We get payment entries of payments received.
+	payment_entry_received = frappe.db.sql("""SELECT posting_date, received_amount, paid_from_account_currency FROM `tabPayment Entry` WHERE payment_type = 'Receive'""", as_dict=True)
+	# We get payment entries of payments made.
+	payment_entry_paid = frappe.db.sql("""SELECT posting_date, paid_amount, paid_to_account_currency FROM `tabPayment Entry` WHERE payment_type = 'Pay'""", as_dict=True)
+	write_file(payment_entry_received, 'develop_payment_entry_received')
+	write_file(payment_entry_paid, 'develop_payment_entry_paid')
+	return payment_entry_received, payment_entry_paid
+
+def get_journal_entries():
+	'''Function that obtains the payment entries from the database. Returns a list of dictionary objects.
+	this function contains a write_file function that will write the results of its execution to a file. Comment it out when
+	functionality has been confirmed.
+	'''
+	# Get journal entry accounts and amounts, for Cash and Bank transactions.
+	journal_entry_accounts = frappe.db.sql("""SELECT parent, account_type, account, account_currency, debit_in_account_currency, credit_in_account_currency FROM `tabJournal Entry Account` WHERE account_type = 'Bank' OR account_type = 'Cash'""", as_dict=True)
+	
+	# For development and debugging: Save contents of data obtained to a file.
+	write_file(journal_entry_accounts, 'develop_journal_entry_accounts')
+	'''
+	parent
+	# To keep track of which journal entry is the parent, so date of journaling can be found.
+	account_type
+	# This is a crucial element. Any account which is not of Cash or Bank type, will be ignored!
+	account
+	# The account being used for the movement
+	account_currency
+	# The currency symbol for the accounting units for that specific journal entry account
+	debit_in_account_currency
+	# Debit in account currency units
+	credit_in_account_currency
+	# Credit in account currency units
+	is_advance
+	# Not immediately useful, but can be used for later segregation in report, so financial managers know which portion of the flows are advance payments (given or received)
+	'''
+	return journal_entry_accounts
+
+def parse_journal_entry(journal_entry):
+	'''[WIP] Function that obtains journal entry account data and segregates amounts received or paid'''
+	# https://www.digitalocean.com/community/tutorials/how-to-handle-plain-text-files-in-python-3
+	
+	
+	
+	# for entry in journal_entry:
+		#obtain dictionary object
+		
+		# read debit amount
+		# read credit amount
+		
+		# if debit = 0, then bank or cash account is credited, which means money paid
+		# if credit = 0, then bank or cash account is debited, which means money received.
+		# should return a payment entry like object (so we can mix them seamlessly and then parse them!
+	return journal_entry_results
+
+def write_file(data_contents, filename):
+	'''Function to write to a text file any data passed to the function, for debugging and development purposes only'''
+	# https://www.digitalocean.com/community/tutorials/how-to-handle-plain-text-files-in-python-3
+	file_path = '/home/alain/sihaysistema/apps/revelare/revelare/revelare/report/direct_cash_flow_report/' + str(filename) + '.txt'
+	new_file = open(file_path,'w')
+	contents = "The file now contains this text written from Python"
+	new_file.write(str(data_contents))
+	new_file.close()
+	return
 
 def get_columns(filters):
 	'''Genera las columnas necesarias para el reporte'''
 
 	columns = [{
 		"fieldname": "name",
-		"label": _("Party Cash Flow"),
+		"label": _("Direct Cash Flow"),
 		"fieldtype": "Link",
 		"options": "Budgeted Cash Flow",
 		"width": 200
 	}]
-
+	# Obtains a list of pairs of datetime objects (start and end periods as per periodicity setting selected by user)
 	ranges = get_period_date_ranges(filters)
 
 	# Genera las columnas en base al rango de fechas
@@ -91,16 +164,18 @@ def get_columns(filters):
 			"fieldtype": "Float",
 			"width": 120
 		})
+		
+	# TODO:  Sort the columns selected, at least for the weeks.
 
 	return columns
 
-
 def get_period_date_ranges(filters):
-	'''Obtiene el periodo en base al rango de fechas'''
-
+	'''Returns the date range according to the initial and final dates'''
+	# Relativedelta - for datetime calculations
 	from dateutil.relativedelta import relativedelta
 	from_date, to_date = getdate(filters.from_date), getdate(filters.to_date)
-
+	
+	# Get the value of the key passed to range. Then Setting the increment value according to the range
 	increment = {
 		"Monthly": 1,
 		"Quarterly": 3,
@@ -108,26 +183,34 @@ def get_period_date_ranges(filters):
 		"Yearly": 12
 	}.get(filters.range,1)
 
+	# We create an empty list to hold the periodic date ranges, once filled up it will be returned.
 	periodic_daterange = []
+	# using range allows for specific amounts of looping. the first number is the start parameter:  1 (not 0). 53 is the ending value. Increment is the interval by which the loop should increment.  These are dummy variables.
 	for dummy in range(1, 53, increment):
 		if filters.range == "Weekly":
+			# when entering the loop for the first time, from date contains user declared from date. On subsequent iterations, it is now the period end date + 1 day, so the new period of dates is calculated.  In this case, the next week.
 			period_end_date = from_date + relativedelta(days=6)
 		else:
+			# when entering the loop for the first time, from date contains user declared "from date". On subsequent iterations, it is now the period end date + 1 day, so the new period of dates is calculated. In this case, the period end date is given the increment chosen above (1 month, 3 months, 6 months, 12 months).
 			period_end_date = from_date + relativedelta(months=increment, days=-1)
-
+			
+		# If the period end date just estimated is greater than the to date (limit date chosen by user), the period end date is set to be the to date. This way, no date range is ever exceeded.
 		if period_end_date > to_date:
 			period_end_date = to_date
+		# Having run all the checks, we finally append, to the list, another list with the from_date and the period end date.
+		
 		periodic_daterange.append([from_date, period_end_date])
-
+		# this creates a new "from date" using the period end date just calculated and adding one day with relativedelta. This sets the stage for the next period.
+		
 		from_date = period_end_date + relativedelta(days=1)
+		# if the period_end date just calculated ends up being 
 		if period_end_date == to_date:
 			break
 
 	return periodic_daterange
 
-
 def get_period(posting_date, filters):
-	'''retorna el periodo en base al filtro del reporte'''
+	'''Returns the period based on the report filter'''
 
 	months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
@@ -142,7 +225,6 @@ def get_period(posting_date, filters):
 		period = str(year[2])
 
 	return period
-
 
 def prepare_data_unpaid(filters):
 	'''Prepara la data en base a los registros pendientes por cobrar
@@ -165,7 +247,7 @@ def prepare_data_unpaid(filters):
 		total = 0
 		for dummy, end_date in ranges:
 			period = get_period(end_date, filters)
-			fecha_registro = conversion_fechas(item_data.posting_date, filters)
+			fecha_registro = date_conversion(item_data.posting_date, filters)
 
 			if fecha_registro == period:
 				amount = flt(item_data.paid_amount)
@@ -180,7 +262,6 @@ def prepare_data_unpaid(filters):
 		data.append(row)
 
 	return data
-
 
 def prepare_data_paid(filters):
 	'''Prepara la data en base a los registros pagados (EGRESOS)
@@ -204,7 +285,7 @@ def prepare_data_paid(filters):
 		total = 0
 		for dummy, end_date in ranges:
 			period = get_period(end_date, filters)
-			fecha_registro = conversion_fechas(item_data.posting_date, filters)
+			fecha_registro = date_conversion(item_data.posting_date, filters)
 
 			# frappe.msgprint(_(period))
 			if fecha_registro == period:
@@ -221,7 +302,6 @@ def prepare_data_paid(filters):
 
 	return data
 
-
 def get_chart_data(columns):
 	'''retorna la data necesaria para mostrar las graficas en el reporte'''
 
@@ -236,11 +316,8 @@ def get_chart_data(columns):
 
 	return chart
 
-
 def get_reg_unpaid(filters):
-	'''Retorna la data solicitada de la base de datos, especificamente los registros
-		pendientes por cobrar
-	'''
+	'''Returns the requested data from the database, in this case the records representing expected future receipts'''
 
 	data_cash_flow = frappe.db.get_values('Budgeted Cash Flow',
 										filters={'company': filters.get("company"), 'status_payment': 'Unpaid'},
@@ -248,7 +325,6 @@ def get_reg_unpaid(filters):
 												'due_date'], as_dict=1)
 
 	return data_cash_flow
-
 
 def get_reg_paid(filters):
 	'''Retorna la data solicitada de la base de datos, especificamente los registros
@@ -262,10 +338,9 @@ def get_reg_paid(filters):
 
 	return data_cash_flow
 
-
-def conversion_fechas(fecha, filters):
-	'''Funcion customizada para trabajar fechas especialmente semanal TODO: daily'''
-
+def date_conversion(fecha, filters):
+	'''Customized function to work with dates, especially weekly date ranges. TODO: Bimonthly (every 15 days), Daily'''
+	'''Will not run if periodicity is monthly.'''
 	if filters.range == 'Weekly':
 		period = 'Week ' + str(fecha.isocalendar()[1]) + ' ' + str(fecha.year)
 	elif filters.range == 'Monthly':
@@ -276,9 +351,10 @@ def conversion_fechas(fecha, filters):
 		# period = str(fecha.year)
 		year = get_fiscal_year(fecha, company=filters.company)
 		period = str(year[2])
+	
+	write_file(period, 'develop_date_conversion_results')
 
 	return period
-
 
 def add_total_row(out, filters, tipo=False):
 	'''Agrega el total de ingresos como egresos'''
@@ -323,7 +399,6 @@ def add_total_row(out, filters, tipo=False):
 	data.append({})
 
 	return data
-
 
 def add_total_row_report(total_in, total_e, filters):
 	'''Total del reporte resta la suma de ingresos con la suma de egresos'''
